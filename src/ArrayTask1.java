3d956fbabeb267b44b1d5d16b286b32d2f6f27a0
McLoy <tkachenko.vlad@hotmail.com>
2016-08-31 18:09:35 +0300

    update

--- src/ArrayTask1.java
+++ src/ArrayTask1.java
@@ -1,8 +1,5 @@
 import java.util.*;
 
-/**
- * Created by grisha on 8/31/16.
- */
 public class ArrayTask1 {
     public static void main(String[] args) {
 
@@ -16,22 +13,28 @@ public class ArrayTask1 {
         for (int i = 0; i< mass.length; i++)
             mass[i] = (int)(Math.random()*5);
 
-        Arrays.sort(mass);
+        //Arrays.sort(mass);
 
         for (int k: mass)
         System.out.println(k);
 
         int count = 0;
+        int doubler = -1;
         for (int i = 0; i< mass.length; i++) {
             for (int j = 0; j < mass.length; j++) {
 
-
                 if (i != j && mass[i] != -1 && mass[i] == mass[j]){
-                    mass[i] = -1;
+
                     mass[j] = -1;
-                    count = count + 1;
+                    if (doubler != mass[i]){
+                        count = count + 1;
+                        doubler = mass[i];
+                    }
+                    if (j == mass.length)
+                        mass[i] = -1;
                 }
             }
+
         }
 
         System.out.println("В массиве " + count + " дублей");
